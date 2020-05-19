5d2ced8e8664d3fa7439487935724080c49ef285
dizangwang <2561179983@qq.com>
2020-05-19 16:01:59 +0800

    修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "eqeqeq": "off", //比较的时候使用严格等于
+    "no-alert": 'error', //不允许使用alert，confirm，prompt语句
+    "no-plusplus": 'off' //不允许使用++ --运算符
   },
 };
