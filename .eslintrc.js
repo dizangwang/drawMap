9a8ca94d70d916cf38419f119c868f7ab408af5f
dizangwang <2561179983@qq.com>
2020-06-24 18:37:49 +0800

    测试

--- .eslintrc.js
+++ .eslintrc.js
@@ -62,7 +62,6 @@ module.exports = {
     "no-unused-vars": 0,
     "no-undef": 0,
     "vars-on-top": 0,
-    "no-mixed-operators":0,
-    "no-unexpected-multiline":0
+    "no-mixed-operators":0
   }
 };
